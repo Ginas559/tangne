974774fa86ac9fb8e14618f7ac7c39916813fd00
Ginas559 <140213980+Ginas559@users.noreply.github.com>
2024-11-20 16:15:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Đặng Ánh Mai';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng cô giáo';
+const contentCard = 'Nhân ngày Nhà giáo Việt Nam 20/11, kính chúc cô thật nhiều sức khỏe, luôn vui vẻ và hạnh phúc.';
+
+const giftImage = 'teacher.png'; // Hình ảnh liên quan đến cô giáo (có thể thay thế bằng hình ảnh phù hợp)
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
